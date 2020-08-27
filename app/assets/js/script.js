a8318b52b9be5643629c7bf31bc5ab6ae4ed12b1
Эльдар Дадашов <eldar-dadashov@mail.ru>
2020-08-27 04:03:08 +0300

    Сверстан footer с адаптивом, добавленны небольшие изменения в остальные блоки

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,8 +1,22 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+let swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  arrows: false,
+  navigation: {
+    nextEl: '.team__next',
+    prevEl: '.team__prev',
+  },
+  breakpoints: {
+    820: {
+      slidesPerView: 2,
+      spaceBetween: 50
     },
+    1300: {
+      slidesPerView: 3,
+      spaceBetween: 100
+    }
+  }
 });
