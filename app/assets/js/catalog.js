ff1013fb6792d388ca94a4b9ab0cdd515ca02bd8
Эльдар Дадашов <eldar-dadashov@mail.ru>
2020-08-28 17:10:29 +0300

    Переписал js для каталога (рейтинг, фильтры)

--- app/assets/js/catalog.js
+++ app/assets/js/catalog.js
@@ -1,25 +1,38 @@
 const catalogFilter = document.querySelector('.catalog-filter');
+const catalogBlocks = document.querySelector('.catalog-blocks');
 
 catalogFilter.addEventListener('click', e => {
+    const target = e.target;
     if(e.target.tagName === 'svg') {
-        e.target.classList.toggle('arrow_180');
-        e.target.parentElement.classList.toggle('catalog_show-list');
-    } 
+        const target = e.target;
+        target.classList.toggle('arrow_180');
+        target.parentElement.classList.toggle('catalog_show-list');
+    }
+    else if(target.classList.contains('select__element')) {
+        const dataActive = target.dataset.value; // летит как data-active
+        const dataText = target.innerHTML; // летит как текст
+        const ul = target.parentElement;
+        const div = ul.parentElement;
+        const arrow = div.querySelector('svg'); 
+        const p = div.querySelector('.catalog-select__check');
+        p.innerHTML = dataText;
+        p.dataset = dataActive;
+        div.classList.toggle('catalog_show-list');
+        arrow.classList.toggle('arrow_180');
+    }
 });
 
-(function(){
-	const ratingBlocks = document.querySelectorAll('.rating');
-    
-    ratingBlocks.forEach(element => {
-        let items = element.querySelectorAll('.rating-item');
-        element.addEventListener('click', e => {
-            if(!e.target.classList.contains('rating_active'))
-            {
-                items.forEach(function(item){
-                    item.classList.remove('rating_active');
-                });
-                e.target.classList.add('rating_active');
-            }
-        });
-    });
-})();
+// Raiting
+catalogBlocks.addEventListener('click', e => {
+    const target = e.target;
+    if(target.classList.contains('rating-item') && !target.classList.contains('rating_active')) {
+        {
+            const rating = target.parentElement;
+            const items =  rating.querySelectorAll('.rating-item');
+            items.forEach(item => {
+                item.classList.remove('rating_active');
+            });
+            target.classList.add('rating_active');
+        }
+    }
+});
