fe413b253cfd464a013d64c4e75fff05b9403050
Эльдар Дадашов <eldar-dadashov@mail.ru>
2020-08-28 22:02:37 +0300

    Улучшил навигационное меню на телефонах

--- app/assets/js/aside.js
+++ app/assets/js/aside.js
@@ -1,15 +1,14 @@
 const menu = document.querySelector('.aside__menu');
 const aside = document.querySelector('.main__aside');
-const allNotAside = document.querySelector('*:not(.main__aside):not(.aside__menu)');
 
 menu.addEventListener('click', () => {
-  menu.style.display = 'none';
   aside.classList.toggle('aside_show');
 });
 
-allNotAside.addEventListener('click', () => {
-  aside.classList.remove('aside_show');
-  menu.style.display = 'block';
+document.addEventListener('click', e => {
+    if(!e.target.classList.contains('aside__menu__item') && !e.target.classList.contains('main__aside')) {
+        aside.classList.remove('aside_show');
+    }
 });
 
 window.addEventListener('resize', () => {
