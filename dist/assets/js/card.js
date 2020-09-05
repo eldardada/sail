6ebcd249dbd5fed16d15c3dbf48262aaf3ded9e6
Эльдар Дадашов <eldar-dadashov@mail.ru>
2020-09-05 13:59:20 +0300

    Валидация форм закончена, попытка сделать реализацию выпадающих списков

--- dist/assets/js/card.js
+++ dist/assets/js/card.js
@@ -13,7 +13,7 @@ function hideFeedbackModal() {
 
 document.addEventListener('click', e => {
     const target = e.target;
-    const its_feedbackModal= target == feedbackModal || feedbackModal.contains(target);
+    const its_feedbackModal = target == feedbackModal || feedbackModal.contains(target);
     const its_feedbackBtn = target == feedbackBtn;
     const feedbackModal_is_active = feedbackModal.classList.contains('show');
 
@@ -88,25 +88,121 @@ cardModelCount.addEventListener('click', e => {
 
 const cardInfoAdd = document.querySelector('.card-info-add');
 
+
+cardAddCheckInputs = document.querySelectorAll('.card-info-add__block-check>input');
+
 cardInfoAdd.addEventListener('click', e => {
     const target = e.target;
+    
+    let div = target;
 
-    if(target.classList.contains('.add__sign')) {
-        
+    while(!div.classList.contains('add__sign') && !div.classList.contains('card-info-add__block-check')) {
+        if(div == cardInfoAdd) {
+            break;
+        }
+        div = div.parentElement;
     }
-    else {
-        let div = target;
 
-        while(!div.classList.contains('.add__sign')) {
-            if(div.parentElement == cardInfoAdd) {
-                break;
-            }
-            div = div.parentElement;
+    if(div.classList.contains('add__sign')) {
+        let divParrent = div.parentElement;
+        const input = divParrent.querySelector('input');
+        if(div.dataset.name == 'plus' && input.value < 50) {
+            input.value = Number(input.value) + 1; 
         }
-        if(div.dataset.name == 'plus') {
+        else if(div.dataset.name == 'minus' && input.value > 0) {
+            input.value -= 1;
+        }
+    }
+    // drop-down lists
+    else if(div.classList.contains('card-info-add__block-check')) {
+        const input = div.querySelector('input');
+        const label = div.querySelector('label');
+        const list = div.querySelector('ul');
+        const itListElement = list.contains(target) && target != list;
+
+        if(itListElement) {
+            label.innerHTML = target.innerHTML;
+            input.checked = true;
+            list.style.transform = 'scaleY(0)';
+        }
+        else if(!input.checked) {
+            text = label.innerHTML;
+            list.style.transform = 'scaleY(1)';
             
+            cardAddCheckInputs.forEach(AddInput => {
+               
+                if(AddInput != input) {
+                    let parrentInput = AddInput.parentElement;
+                    let ul = parrentInput.querySelector('ul');
+                    ul.style.transform = 'scaleY(0)';
+                } 
+            });
         }
-
+        else if(list.style.transform != 'scaleY(1)') {
+            input.checked = false;
+            list.style.transform = 'scaleY(1)';
+            if(input.id == 'stand')
+                label.innerHTML = 'Подставка';
+            else if(input.id == 'basket')
+                label.innerHTML = 'Корзина';
+            else if(input.id == 'pillow')
+                label.innerHTML = 'Подушка';
+            else if(input.id == 'casePillow')
+                label.innerHTML = 'Чехол подушка';
+            else if(input.id == 'chain')
+                label.innerHTML = 'Цепь';
+            else if(input.id == 'armСhair')
+                label.innerHTML = 'Цепь';
+        }
     }
 });
+
+cardInfoAdd.addEventListener('input', e => {
+    const target = e.target;
+    // plus / minus
+    if(target.parentElement.classList.contains('card-info-add__block-count')) {
+        target.value = target.value.replace(/\D/g, '');
+    }
+   
+});
+
+cardInfoAdd.addEventListener('change', e => {
+    const target = e.target;
+    if(target.parentElement.classList.contains('card-info-add__block-count')) {
+       if(target.value === '') {
+            target.value = 0;
+       }
+       else if (target.value > 50) {
+            target.value = 50;
+       }
+    }
+});
+
+// feedback
+
+const feedbackBlock = document.querySelector('.feedback');
+
+feedbackBlock.addEventListener('click', e => {
+    let target = e.target;
+
+    if(!target.classList.contains('like') && !target.classList.contains('dislike')) {
+      target = target.parentElement;  
+    }
+
+    if(target.classList.contains('like') || target.classList.contains('dislike')) {
+        let div = target;
+        
+        while(!div.classList.contains('feedback-bgblock')) div = div.parentElement;
+
+        if(target.classList.contains('like')) {
+            div.querySelector('.dislike').style.fill = 'black';
+            target.style.fill = 'green';
+        }
+        else if(target.classList.contains('dislike')) {
+            div.querySelector('.like').style.fill = 'black';
+            target.style.fill = 'red';
+        }
+    }
+});
+
 
